cc69db0ddedbc045f7bab3f20d591f3fc5c580fa
Coco-tobio <97827297+Coco-tobio@users.noreply.github.com>
2022-01-17 14:59:25 +0530

    greatest between x, y, z
    operator use

---
+++ greatestin xyz.py
@@ -0,0 +1,10 @@
+x=y=z=0
+x=int(input("enter first no.:"))
+y=int(input("enter second no.:"))
+z=int(input("enter third no.:"))
+if x>y and x>z:
+    print("the largest no. is:",x)
+elif y>z and y>z:
+    print("the largest no is:",y)
+elif z>x and z>y:
+    print("the largest no is:",z)
